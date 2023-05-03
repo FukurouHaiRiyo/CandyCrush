5fbdc74afd5df9a2afa51336c6bc39a95ff98553
FukurouHaiRiyo <andreipanait00@gmail.com>
2023-05-03 19:43:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import LevelPage from './components/LevelPage';
 const App = () => {
   return (
     <BrowserRouter>
-      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded sm:py-3 sm:px-6 md:py-4 md:px-8 lg:py-5 lg:px-10'>
-        <Link to="/landing-page">Start playing</Link>
-      </button>
+        <Link to="/landing-page">
+          <button className='w-full rounded-lg p-4 text-lg font-bold bg-green-500 hover:bg-green-600 text-white'>
+              Start playing
+          </button>
+        </Link>
+      
       <Routes>
         <Route path="/landing-page" element={<LandingPage />} />
         <Route path="/level/:levelId" element={<LevelPage />} />
